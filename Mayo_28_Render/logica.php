2be9b1f470ef34c596f2367e4701b16469b11e6c
saenzkarol82-code <saenzkarol82@gmail.com>
2026-05-28 19:46:19 -0500

    Update logica.php

--- Mayo_28_Render/logica.php
+++ Mayo_28_Render/logica.php
@@ -6,14 +6,14 @@ $hoy = date("Y-m-d H:i:s");
 require 'vendor/autoload.php'; // Cargar Composer
 
     $cliente = new MongoDB\Client("Cadena de conexion a servicio Mongo Atlas");
-    $db = $cliente->sena;	// Nombre de BD
-    $coleccion = $db->aprendices;	//Nombre de la coleccion	
+    $db = $cliente->prueba;	// Nombre de BD
+    $coleccion = $db->gustos;	//Nombre de la coleccion	
     $resultado = $coleccion->insertOne([
         "apellidos" => $_POST["apellidos"],
         "nombres" => $_POST["nombres"],
         "color" => $_POST["color"],
         "comida" => $_POST["comida"],
-        "pelicula" => $_POST["pelicula"],
+        "pelicula" => $_POST["cine"],
         "registro" => $hoy
     ]);
     echo "<center><h3 style='border:1px solid green;background-color:rgb(64,145,108);color:white;padding:1%;'>Documento insertado con ID: " . $resultado->getInsertedId() . "</h3></center>";
